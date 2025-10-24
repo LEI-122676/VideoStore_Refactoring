2f8dbca0533311091d5b38f255ca641f65dc8ed0
LEI-122676 <tfcss2@iscte-iul.pt>
2025-10-24 17:14:57 +0100

    v2-v3 Extract Method e Replace Temp with Query

--- src/ficha3/Customer.java
+++ src/ficha3/Customer.java
@@ -32,18 +32,14 @@ public class Customer
 		
 		for (Rental each: _rentals)
 		{
-            double thisAmount = each.getAmount();
-
             // add frequent renter points
 			frequentRenterPoints++;
 
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
+            frequentRenterPoints = getFrequentRentalPoints(each, frequentRenterPoints);
 
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
+            // show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
+			totalAmount += each.getAmount();
 		}
 
 		// add footer lines
@@ -51,5 +47,12 @@ public class Customer
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
 	}
+
+    private static int getFrequentRentalPoints(Rental each, int frequentRenterPoints) {
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 
 }
